b0d2639a022d26d70d1502c5a1acb67aefcc5aa8
Long Phun <longphun90@yahoo.com>
2022-11-16 17:05:56 -0800

    redux store persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,16 @@
 import { compose, legacy_createStore as createStore } from "redux";
-
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 
-export const store = createStore(rootReducer, undefined)
+//persist redux state(informations) on app refresh
+const perssitConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(perssitConfig, rootReducer)
+
+export const store = createStore(persistedReducer, undefined)
+
+export const persistor = persistStore(store)
